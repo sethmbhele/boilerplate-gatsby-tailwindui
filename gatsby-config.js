719e0df45d55c909ded732d8d1a05d2b6854e8d3
Seth Mbhele <seth@immersiongroup.com>
2021-05-02 21:06:47 +0200

    Added tailwind, purgecss, postcss, autoprefixer, tailwindui and heroicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,18 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: ‘gatsby- plugin - postcss’,
+    options: {
+      postCssPlugins: [require(“tailwindcss”), require(“autoprefixer)],
+    },
+    {
+      resolve: ‘gatsby- plugin - purgecss’,
+    options: {
+      printRejected: false,
+      develop: false,
+      tailwind: true,
+    },
+  ],
 }
