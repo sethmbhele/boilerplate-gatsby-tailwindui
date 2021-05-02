779e938a2718b59504e60b711786b077014c92ea
Seth Mbhele <seth@immersiongroup.com>
2021-05-02 22:46:12 +0200

    Added tailwind default colours and index page + mminor bugfixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,16 +8,18 @@ module.exports = {
   /* Your site config here */
   plugins: [
     {
-      resolve: ‘gatsby- plugin - postcss’,
-    options: {
-      postCssPlugins: [require(“tailwindcss”), require(“autoprefixer)],
+      resolve: 'gatsby-plugin-postcss',
+      options: {
+        postCssPlugins: [require("tailwindcss"), require("autoprefixer")],
+      },
     },
     {
-      resolve: ‘gatsby- plugin - purgecss’,
-    options: {
-      printRejected: false,
-      develop: false,
-      tailwind: true,
+      resolve: 'gatsby-plugin-purgecss',
+      options: {
+        printRejected: false,
+        develop: false,
+        tailwind: true,
+      },
     },
   ],
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,37 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./src/**/*.{js, jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      bluegrey: colors.blueGray,
+      coolgrey: colors.coolGray,
+      grey: colors.gray,
+      truegrey: colors.trueGray,
+      warmgrey: colors.warmGray,
+      red: colors.red,
+      orange: colors.orange,
+      amber: colors.amber,
+      yellow: colors.yellow,
+      lime: colors.lime,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      lightblue: colors.lightBlue,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      violet: colors.violet,
+      purple: colors.purple,
+      fuschia: colors.fuchsia,
+      pink: colors.pink,
+      rose: colors.rose,
+    },
     extend: {},
   },
   variants: {
